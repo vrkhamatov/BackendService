f2c41215f6792b6b328f58e055a4c1545886648c
Vadim Khamatov <74698624+vrkhamatov@users.noreply.github.com>
2023-03-23 04:24:39 +0300

    31

--- BackendService/src/main/java/com/example/backendservice/controller/BackendController.java
+++ BackendService/src/main/java/com/example/backendservice/controller/BackendController.java
@@ -33,18 +33,18 @@ public class BackendController {
     Environment env; //get path to file directory from command line
 
     // command: //java -jar target/BackendService-0.0.1-SNAPSHOT.jar --path=C:\Users\khama\Desktop\LetiPrint\BackendService\src\main\resources\storage
-//    @GetMapping("/downloadFile")
-//    @ResponseBody
-//    @ExceptionHandler
-//    private ResponseEntity<?> getFile(@RequestParam String fileId) {
-//        try {
-//            MetaData metaData = metaDataRepository.getMetaDataByFileId(fileId);
-//            return new ResponseEntity<>(jsonSerelizator.serializeJsonForIoTService(metaData), HttpStatus.OK);
-//        }
-//        catch (Exception e) {
-//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//        }
-//    }
+   @GetMapping("/downloadFile")
+   @ResponseBody
+   @ExceptionHandler
+   private ResponseEntity<?> getFile(@RequestParam String fileId) {
+       try {
+           MetaData metaData = metaDataRepository.getMetaDataByFileId(fileId);
+           return new ResponseEntity<>(jsonSerelizator.serializeJsonForIoTService(metaData), HttpStatus.OK);
+       }
+       catch (Exception e) {
+           return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+       }
+   }
 
     @PostMapping("/metaData")
     public String getUsersId(@RequestBody String metaData) {
